ed4e48ee2ba61c1033d67c4085cf049197f8bd15
JosipSkrlec <jskrlec1997@gmail.com>
2021-09-16 18:46:11 +0200

    Added saving file to rootfolder, added table view, pdf reader...

--- Vjezba.Web/Controllers/RacunController.cs
+++ Vjezba.Web/Controllers/RacunController.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using Vjezba.DAL;
 using Vjezba.Model;
 using Vjezba.Web.Models;
@@ -20,6 +22,8 @@ namespace Vjezba.Web.Controllers
     {
         //private static string filePathForDB;
         private RacunModelDbContext _dbContext;
+
+        public static int pdfCounterHelperIndex = 0;
 
         public RacunController(RacunModelDbContext dbContext, UserManager<AppUser> userManager) : base(userManager)
         {
@@ -117,8 +121,98 @@ namespace Vjezba.Web.Controllers
 
             pdf.GenerirajPDF(proizvodList);
 
+            pdfCounterHelperIndex++;
+            var pdfPodaciByteArray = pdf.Podaci;
+            System.IO.File.WriteAllBytes(@"wwwroot\PDFs\Racunstavka" + pdfCounterHelperIndex + ".pdf", pdfPodaciByteArray);
+
+            //FileStream fileStream = new FileStream("RacunStavke", FileMode.Create, FileAccess.ReadWrite);
+            //fileStream.Write(pdfPodaciByteArray, 0, pdfPodaciByteArray.Length);
+            //fileStream.Close();
+
             return File(pdf.Podaci, System.Net.Mime.MediaTypeNames.Application.Pdf, "RacunStavke.pdf");
         }
+
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult PDFList()
+        {
+            string[] filePaths = Directory.GetFiles(@"wwwroot\PDFs\", "", SearchOption.AllDirectories);
+
+            return View("PDFList", filePaths);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult ShowPDF(string pdfPath)
+        {
+            if (pdfPath == null)
+            {
+                return RedirectToAction();
+            }
+            else
+            {
+
+                //var folderPath = Path.Combine(pdfPath);
+
+                //if (Directory.Exists(folderPath))
+                //{
+                //    var fileCount = Directory.GetFiles(@folderPath, "*.pdf");
+                //    if (fileCount.Length > 1) // more than 1 file
+                //    {
+                //        Process.Start(folderPath);
+                //    }
+                //    else if (fileCount.Length == 1) // exactly only 1 file
+                //    {
+                //        //if(TempData["SingleFileName"] != null)
+                //        //{
+                //        //    string name = TempData["SingleFileName"] as string;
+
+                //        return File(folderPath + "\\" + pdfPath, "application/pdf");
+                //        //}
+                //    }
+                //    else // no files 
+                //    {
+                //        // maybe make alert message says apparently has no files ?!@ ?! !?!?? ??
+                //        Process.Start(folderPath);
+                //    }
+
+                //}
+
+                //Stream stream = null;
+                //FileStream fstream = null;
+                //if (System.IO.File.Exists(pdfPath) == false)
+                //{
+                //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(pdfPath);
+                //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //    stream = response.GetResponseStream();
+
+                //    //byte[] inBuffer = ReadFully(stream);
+                //    byte[] FileBytes = System.IO.File.ReadAllBytes(pdfPath);
+                //    fstream = new FileStream(pdfPath, FileMode.OpenOrCreate, FileAccess.Write);
+                //    fstream.Write(FileBytes, 0, FileBytes.Length - 1);
+
+
+                //    fstream.Close();
+                //    stream.Close();
+                //}
+
+
+                //string fullPath = Path.Combine(@"", pdfPath);
+
+                //return the file for download, this is an Excel 
+                //so I set the file content type to "application/vnd.ms-excel"
+                //return File(fullPath, "DownloadedPDF", pdfPath);
+
+
+                //Read the File data into Byte Array.
+                byte[] bytes = System.IO.File.ReadAllBytes(pdfPath);
+
+                //Send the File to Download.
+                return File(bytes, "application/octet-stream", "RacunPregledPDF.pdf");
+
+                //return RedirectToAction();
+            }
+        }
+
 
         //[Authorize(Roles = "Admin,User")]
         public IActionResult Details(int? id = null)
@@ -165,19 +259,19 @@ namespace Vjezba.Web.Controllers
             return PartialView("_IndexTable"/*, model: model*/);
         }
 
-        public void UploadObjFile(IFormFile file)
-        {
-            var FileDic = @"wwwroot\3DModels";
-            var fileName = file.FileName;
-            var filePath = Path.Combine(FileDic, fileName);
+        //public void UploadObjFile(IFormFile file)
+        //{
+        //    var FileDic = @"wwwroot\3DModels";
+        //    var fileName = file.FileName;
+        //    var filePath = Path.Combine(FileDic, fileName);
 
-            //filePathForDB = "/3DModels/" + fileName;
+        //    //filePathForDB = "/3DModels/" + fileName;
 
-            using (FileStream fs = System.IO.File.Create(filePath))
-            {
-                file.CopyTo(fs);
-            }
-        }
+        //    using (FileStream fs = System.IO.File.Create(filePath))
+        //    {
+        //        file.CopyTo(fs);
+        //    }
+        //}
         private void FillDropdownValues()
         {
             var selectItems = new List<SelectListItem>();
